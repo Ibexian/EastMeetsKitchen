3e0ce7917fdfd8413dc9fa9de5eec82b84e07201
William Kamovitch <wjkamovitch@gmail.com>
2021-11-29 19:35:48 +0000

    Update gatsby version and improve gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
+import ImageZoom from "../ImageZoom/ImageZoom";
 import "./Gallery.css";
+
 
 class Gallery extends Component {
   render() {
-    const images = [...Array(45).keys()].slice(1)
+    console.log(this.props.images)
+    const images = this.props.images;
     return (
       <div className="gallery">
         <ul>
           {images.map( image => (
-            <li>
-              <img src={`/gallery/${image}.jpg`} alt="" />
+            <li style={{width: `calc(50vh * ${image.node.childImageSharp.fluid.aspectRatio})`, maxWidth: `calc(600px * ${image.node.childImageSharp.fluid.aspectRatio})`}}>
+              <ImageZoom fluid={image.node.childImageSharp.fluid}/> 
             </li>
           ))}
         </ul>
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -4,6 +4,23 @@ import Layout from "../layout";
 import Gallery from "../components/Gallery/Gallery";
 import config from "../../data/SiteConfig";
 
+export const query = graphql`
+  query {
+    allFile(sort: {order: ASC, fields: absolutePath}, filter: {absolutePath: {regex: "/gallery-(([01]?[0-9])|(20))*/"}}) {
+      edges {
+        node {
+          childImageSharp{
+            fluid(maxHeight: 1000
+              quality: 90) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 
 class GalleryPage extends Component {
   render() {
@@ -11,7 +28,7 @@ class GalleryPage extends Component {
       <Layout>
         <div className="about-container">
           <Helmet title={`Gallery | ${config.siteTitle}`} />
-          <Gallery  />
+          <Gallery images={this.props.data.allFile.edges}/>
         </div>
       </Layout>
     );
